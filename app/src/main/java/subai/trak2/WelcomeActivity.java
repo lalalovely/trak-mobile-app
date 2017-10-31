da77984212802a83059d75308d87a84c0099d5b9
lovelygrace <lovearsolon@gmail.com>
2017-10-31 15:14:04 +0800

    with welcome and onboarding pages

--- app/src/main/java/subai/trak2/WelcomeActivity.java
+++ app/src/main/java/subai/trak2/WelcomeActivity.java
@@ -3,6 +3,8 @@ package subai.trak2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -17,6 +19,9 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         tv = (TextView) findViewById(R.id.welcomeText);
         iv = (ImageView) findViewById(R.id.welcomeImage);
