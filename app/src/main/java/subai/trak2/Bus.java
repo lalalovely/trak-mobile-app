fc0808b9d99fc25e4c96e282b2e4338ea2bd88f3
arvin <arvinb450@gmail.com>
2017-11-02 16:17:15 +0800

    update

--- app/src/main/java/subai/trak2/Bus.java
+++ app/src/main/java/subai/trak2/Bus.java
@@ -27,21 +27,14 @@ public class Bus {
         status = "In-transit";
     }
 
-
-    public void setBusDetails(String lat, String lng, String route, String acc, String busComp){
-        destination = new Coordinates(String.valueOf(10.298237), String.valueOf(123.893133));
-        position = new Coordinates(lat,lng);
-        this.route = route;
-        this.accommodation = acc;
-        this.busCompany = busComp;
-    }
-
     //lat, lng, busNumber, route, accommodation, busCompany
 
     public void setStatus(String st) {
         status = st;
     }
+
     public void setPosition(String lat, String lng) {position = new Coordinates(lat,lng);}
+
     public void setAccommodation(String accommodation) {this.accommodation = accommodation;}
 
     public void setBusCompany(String busCompany) {
