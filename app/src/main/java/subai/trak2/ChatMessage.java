9a2b2cb7d9b3bc67f8e46affe117c75e69016921
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2017-11-19 16:49:56 +0800

    - commiting the messaging part of the

---
+++ app/src/main/java/subai/trak2/ChatMessage.java
@@ -0,0 +1,68 @@
+package subai.trak2;
+
+/**
+ * Created by - on 15/11/2017.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Date;
+
+/**
+ * Created by - on 15/11/2017.
+ */
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(){
+        messageText = "default";
+        messageUser = "default_user";
+        messageTime = new Date().getTime();
+    }
+
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage(String messageText){
+        this.messageText = messageText;
+        this.messageUser = "bruhh";
+        messageTime = new Date().getTime();
+    }
+
+    public void setChat(ChatMessage chat){
+        this.messageText = chat.messageText;
+        this.messageUser = chat.messageUser;
+        this.messageTime = chat.messageTime;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
--- app/src/main/java/subai/trak2/MessagingTab.java
+++ app/src/main/java/subai/trak2/MessagingTab.java
@@ -1,17 +1,93 @@
 package subai.trak2;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MessagingTab extends Fragment {
+
+    private static final String TAG = "@string/messaging_tab";
+    private EditText editMessage;
+    private DatabaseReference mRef;
+    private RecyclerView messageList;
+    private FloatingActionButton send;
+    private ChatMessage chat;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.messaging_frag, container, false);
+        editMessage = (EditText) v.findViewById(R.id.edit_txt_msg);
+        mRef = FirebaseDatabase.getInstance().getReference().child("Messages");
+        messageList = (RecyclerView) v.findViewById(R.id.chat_view);
+        send = (FloatingActionButton) v.findViewById(R.id.sendButton);
+        chat = new ChatMessage();
+
+        //setting the message list
+        messageList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext());
+        linearLayoutManager.setStackFromEnd(true);
+        messageList.setLayoutManager(linearLayoutManager);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final String messageValue = editMessage.getText().toString().trim();
+                if (!TextUtils.isEmpty(messageValue)) {
+                    chat.setMessageText(messageValue);
+                    final DatabaseReference newPost = mRef.push();
+                    newPost.child("content").setValue(messageValue);
+                }
+            }
+        });
+
         return v;
     }
-}
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<ChatMessage, MessageViewHolder> FBRA = new FirebaseRecyclerAdapter<ChatMessage, MessageViewHolder>(
+                ChatMessage.class,
+                R.layout.list_item,
+                MessageViewHolder.class,
+                mRef
+        ) {
+                @Override
+                protected void populateViewHolder(MessageViewHolder viewHolder, ChatMessage model, int position) {
+                    model.setChat(chat);
+                    viewHolder.setContent(model.getMessageText());
+                }
+            };
+            messageList.setAdapter(FBRA);
+        }
+
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+        public MessageViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setContent(String content){
+            TextView message_content = (TextView) mView.findViewById(R.id.message_text);
+            message_content.setText(content);
+        }
+    }
+ }
+
