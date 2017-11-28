a7445ac01d2fb54d247c4c503ac04cc67596f537
lovelygrace <lovearsolon@gmail.com>
2017-11-28 22:54:30 +0800

    --

--- app/src/main/java/subai/trak2/MessagingTab.java
+++ app/src/main/java/subai/trak2/MessagingTab.java
@@ -34,7 +34,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-
 public class MessagingTab extends Fragment {
 
     private static final String TAG = "@string/messaging_tab";
